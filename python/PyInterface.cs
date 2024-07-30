0d6ac75dc71ce430962088bb7b3a40da31bdd01a
BetaFoprhoton <96339528+BetaFoprhoton@users.noreply.github.com>
2024-07-30 19:47:11 +0800

    initialize

---
+++ python/PyInterface.cs
@@ -0,0 +1,20 @@
+﻿using Python.Runtime;
+/*
+string pathToVirtualEnv = ".\\envs\\pythonnetTest";
+Runtime.PythonDLL = Path.Combine(pathToVirtualEnv, "python39.dll");
+PythonEngine.PythonHome = Path.Combine(pathToVirtualEnv, "python.exe");
+PythonEngine.PythonPath = $"{pathToVirtualEnv}\\Lib\\site-packages;{pathToVirtualEnv}\\Lib";
+PythonEngine.Initialize();
+
+using (Py.GIL()) 
+{
+    dynamic np = Py.Import("test");
+    np.hello();
+}
+using (Py.GIL())
+{
+    dynamic np = Py.Import("test"); 
+    int r = np.add(1, 2);
+    var a = np.sin;
+    Console.WriteLine($"计算结果{r}");
+}*/
